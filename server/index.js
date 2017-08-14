9ef6f38d01cc8e8900aa51e01d449ed9639af223
Jonathan Sue <hello@jonosue.com>
2017-08-14 20:48:41 +0000

    Fixed issue with connection. Re-added hardcoded port.

--- server/index.js
+++ server/index.js
@@ -3,12 +3,16 @@ require('dotenv').config();
 
 // Basic express and MongoDB setup:
 
-const PORT          = process.env.PORT || 8080;
+// For use with Heroku:
+// const PORT          = process.env.PORT || 8080;
+const PORT          = 8080;
 const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
 const MongoClient   = require("mongodb").MongoClient;
-const MONGODB_URI   = process.env.MONGODB_URI;
+// For use with Heroku:
+// const MONGODB_URI   = process.env.MONGODB_URI;
+const MONGODB_URI   = "mongodb://localhost:27017/tweeter"
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
